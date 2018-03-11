5c20bef85f6d5f353a06cff0142318c99b3d970d
Aleš Mráz <alik810@seznam.cz>
2018-03-11 08:31:44 +0100

    Change camera view

--- src/Graphs/Graphy.js
+++ src/Graphs/Graphy.js
@@ -357,7 +357,7 @@ class Graphy extends Component {
                 center: {
                   x: 0, y: 0, z: 0},
                 eye: {
-                  x:2.5, y:0.1, z:0.1},
+                  x:1.2, y:2.1, z:0.1},
                 up: {
                   x: 0, y: 0, z: 1}
               },
